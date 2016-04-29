01310531ee427f48887ff59a26c651ef3d2f58a1
cthedev <chrismacco8309@gmail.com>
2016-04-28 20:23:10 -0400

    Initial Code Commit
    The first commit of my website with code in it. Very beginning of my
    code. Mostly following the templates, which I plan to change.

---
+++ MyWebApp/MyWebApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About me webpage.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact me webpage.";
+
+            return View();
+        }
+
+        public ActionResult WorkHistory()
+        {
+            ViewBag.Message = "My work history.";
+
+            return View(); 
+        }
+
+    }
+}
